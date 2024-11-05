d8250af523e63cf62926de828a74b39f3b326ea2
Rilsosing <53212931+chimmykk@users.noreply.github.com>
2024-11-06 02:15:15 +0530

    start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 // pages/index.js
 import Head from 'next/head';
-import DeployComponent from '../../components/DeployComponent';
-import MintComponent from '../../components/MintComponent';
+
 import UploadComponent from './components/UploadComponent'; // Import the new component
 
 export default function Home() {
@@ -17,7 +16,7 @@ export default function Home() {
         <h1 style={{ textAlign: 'center' }}>Mintpad v2 Test Deployment</h1>
         <div style={{ marginBottom: '40px' }}>
           <p style={{ textAlign: 'center', fontSize: '18px', fontWeight: 'bold' }}>Deploy Collection</p>
-          <DeployComponent />
+  
         </div>
         <div style={{ marginBottom: '40px' }}>
           <p style={{ textAlign: 'center', fontSize: '18px', fontWeight: 'bold' }}>Upload Folder</p>
@@ -25,7 +24,7 @@ export default function Home() {
         </div>
         <div style={{ marginBottom: '40px' }}>
           <p style={{ textAlign: 'center', fontSize: '18px', fontWeight: 'bold' }}>Mint NFTs</p>
-          <MintComponent />
+   
         </div>
       </main>
 
